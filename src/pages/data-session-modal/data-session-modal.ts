0f3cdff6b80525c0b236f17655325ed1d012a7ca
GUILHEM BRUNEL <Bru@MacBook-Pro-de-GUILHEM.local>
2017-05-21 17:53:55 +0200

    color marker pin

--- src/pages/data-session-modal/data-session-modal.ts
+++ src/pages/data-session-modal/data-session-modal.ts
@@ -19,71 +19,89 @@ export class DataSessionModal {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private sqlite : SQLite, private viewCtrl:ViewController) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DataSessionModal');
-    this.id_session = this.navParams.get('idsession');
-    this.startsession(this.id_session);
-
-  }
-
-  initmap(markerlist){
-
-    var lat = markerlist[0].latitude;
-    var lng = markerlist[0].longitude;
-    var center = {lat: lat, lng: lng};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 20,
-      center: center
-    });
-
-    var marker;
-
-    for (let variable of markerlist) {
-      var uluru = {lat: variable.latitude, lng: variable.longitude};
-      marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-      });
     }
-   }
 
-  startsession(ids){
-    this.sqlite.create({
-      name: 'data.db',
-      location: 'default'
-    })
-    .then((db: SQLiteObject) => {
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad DataSessionModal');
+      this.id_session = this.navParams.get('idsession');
+      this.startsession(this.id_session);
 
-      db.executeSql('CREATE TABLE IF NOT EXISTS datasession(id INTEGER PRIMARY KEY AUTOINCREMENT,id_session,apex,latitude,longitude,hour)', {})
-      .then(() => console.log('Executed SQL'))
-      .catch(e => console.log(e));
+    }
 
-      db.executeSql('select * from datasession where id_session = ? ', [ids]).then((data) => {
-        console.log(JSON.stringify(data));
-        if(data.rows.length > 0) {
-          this.dataList = [];
-          for(var i = 0; i < data.rows.length; i++) {
-            this.dataList.push({
-              id:data.rows.item(i).id,
-              id_session: data.rows.item(i).id_session,
-              apex: data.rows.item(i).apex,
-              latitude: data.rows.item(i).latitude,
-              longitude: data.rows.item(i).longitude,
-              hour: data.rows.item(i).hour
+    initmap(markerlist){
+
+      var lat = markerlist[0].latitude;
+      var lng = markerlist[0].longitude;
+      var center = {lat: lat, lng: lng};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 20,
+        center: center
+      });
+
+      var marker;
+
+      for (let variable of markerlist) {
+        var position = {lat: variable.latitude, lng: variable.longitude};
+        // var apex = markerlist[0].apex;
+        if (variable.apex == "R") {
+          marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+          });
+        } else {
+          if (variable.apex == "P") {
+            marker = new google.maps.Marker({
+              position: position,
+              map: map,
+              icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+            });
+          } else {
+            marker = new google.maps.Marker({
+              position: position,
+              map: map,
+              icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
             });
           }
-          this.initmap(this.dataList);
         }
-      }).catch(e => console.log(e));
+      }
+    }
 
-      return true;
-    })
-    .catch(e => console.log(JSON.stringify(e)));
-  }
+    startsession(ids){
+      this.sqlite.create({
+        name: 'data.db',
+        location: 'default'
+      })
+      .then((db: SQLiteObject) => {
 
-  stopsesion(){
-    this.viewCtrl.dismiss();
+        db.executeSql('CREATE TABLE IF NOT EXISTS datasession(id INTEGER PRIMARY KEY AUTOINCREMENT,id_session,apex,latitude,longitude,hour)', {})
+        .then(() => console.log('Executed SQL'))
+        .catch(e => console.log(e));
+
+        db.executeSql('select * from datasession where id_session = ? ', [ids]).then((data) => {
+          console.log(JSON.stringify(data));
+          if(data.rows.length > 0) {
+            this.dataList = [];
+            for(var i = 0; i < data.rows.length; i++) {
+              this.dataList.push({
+                id:data.rows.item(i).id,
+                id_session: data.rows.item(i).id_session,
+                apex: data.rows.item(i).apex,
+                latitude: data.rows.item(i).latitude,
+                longitude: data.rows.item(i).longitude,
+                hour: data.rows.item(i).hour
+              });
+            }
+            this.initmap(this.dataList);
+          }
+        }).catch(e => console.log(e));
+
+        return true;
+      })
+      .catch(e => console.log(JSON.stringify(e)));
+    }
+
+    stopsesion(){
+      this.viewCtrl.dismiss();
+    }
   }
-}
